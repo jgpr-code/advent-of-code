d572e9d8186b803d56d1bb3b098339f1fef65476
Jörg Planner <joerg.planner@methodpark.de>
2022-11-30 22:09:05 +0100

    solve aoc2021 day13 part1

--- AoC_2021/Day13_TransparentOrigami_Rust/src/main.rs
+++ AoC_2021/Day13_TransparentOrigami_Rust/src/main.rs
@@ -1,93 +1,188 @@
+use anyhow::Result;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::{HashSet, VecDeque};
 use std::io::{self, Read};
 
-fn main() {
-    let mut stdin = io::stdin();
-    let mut buffer = String::new();
-    stdin
-        .read_to_string(&mut buffer)
-        .expect("failed to read file");
-    let mut input_part_one = parse_buffer(&buffer);
-    let mut input_part_two = input_part_one.clone();
-    println!("{:?}", input_part_one);
-    println!("Part 1: {}", part_one(&mut input_part_one));
-    println!("Part 2: {}", part_two(&mut input_part_two));
+struct Input {
+    dots: HashSet<Dot>,
+    instructions: VecDeque<FoldingInstruction>,
 }
 
-struct Grid {
-    content: Vec<Vec<char>>,
-}
-
-impl Grid {
-
-}
-
-#[derive(Debug, Clone)]
 struct Origami {
-    grid: Grid,
-    instructions: Vec<FoldingInstruction>,
+    dots: HashSet<Dot>,
+    instructions: VecDeque<FoldingInstruction>,
+}
+
+impl From<&Input> for Origami {
+    fn from(input: &Input) -> Self {
+        Origami {
+            dots: input.dots.clone(),
+            instructions: input.instructions.clone(),
+        }
+    }
 }
 
 impl Origami {
-    fn execute_instruction(&mut self, instruction: FoldingInstruction) {
-        match instruction {
-            FoldingInstruction::X(fold_at) => {
-                // split at X
-                // vertically_flip right part
+    fn count_dots(&self) -> i128 {
+        self.dots.len() as i128
+    }
+    fn execute_instruction(&mut self) {
+        let mut new_dots = HashSet::new();
+        if let Some(instruction) = self.instructions.pop_front() {
+            for dot in &self.dots {
+                new_dots.insert(instruction.fold_dot(dot));
             }
-            FoldingInstruction::Y(fold_at) => {}
+            self.dots = new_dots;
         }
     }
+}
 
-    fn flip(grid: Grid, axis: usize) -> Grid {
-        grid.clone()
-    }
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Dot {
+    x: i128,
+    y: i128,
+}
 
-    fn split(&self, axis: usize, at: usize) -> (Grid, Grid) {
-        (grid, grid)
-    }
-
-    fn fold(&mut self, grid: Grid, axis: usize, at: usize) -> Grid {
-        let (a, b) = self.split(axis, at);
-        let new_grid = merge(a, b.flip(axis))
-    }
-
-    fn split_grid(grid: Grid, folding_instruction: FoldingInstruction) -> (Grid, Grid) {
-        (grid, grid)
-    }
-
-    fn merge_grids(grid1: Grid, grid2: Grid) -> Grid {
-        let merged = grid1.clone();
-        let rows = merged.len();
-        let cols = merged[0].len();
-        for row in 0..rows {
-            for col in 0..cols {}
+impl From<&str> for Dot {
+    fn from(dot_str: &str) -> Self {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"(\d+),(\d+)").unwrap();
         }
-        merged
-    }
-
-    fn merge_dots(dot1: char, dot2: char) -> char {
-        match (dot1, dot2) {
-            ('#', _) => '#',
-            (_, '#') => '#',
-            _ => '.',
+        let captures = RE.captures(dot_str).unwrap();
+        Dot {
+            x: i128::from_str_radix(&captures[1], 10).unwrap(),
+            y: i128::from_str_radix(&captures[2], 10).unwrap(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
 enum FoldingInstruction {
-    X(usize),
-    Y(usize),
+    X(i128),
+    Y(i128),
 }
 
-fn parse_buffer(buffer: &str) -> Origami {
-    Origami {}
+impl From<&str> for FoldingInstruction {
+    fn from(instruction_str: &str) -> Self {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"fold along ([xy])=(\d+)").unwrap();
+        }
+        let captures = RE.captures(instruction_str).unwrap();
+        match &captures[1] {
+            "x" => FoldingInstruction::X(i128::from_str_radix(&captures[2], 10).unwrap()),
+            "y" => FoldingInstruction::Y(i128::from_str_radix(&captures[2], 10).unwrap()),
+            _ => panic!(),
+        }
+    }
 }
 
-fn part_one(origami: &mut Origami) -> i64 {
-    0
+impl FoldingInstruction {
+    // fold on Y = 7
+    // only dots Y > 7 are affected
+    // dot (x, 10) for example
+    // 10 - (7+1) = 2
+    // (7-1) - (10 - (7+1)) = 4
+    //
+    // new coordinate becomes (x, 4)
+    //
+    // general formula:
+    // fa - 1 - (a - (fa + 1))
+    // fa - 1 - a + fa + 1
+    // 2fa - a
+    fn fold_dot(&self, dot: &Dot) -> Dot {
+        match (self, dot) {
+            (&FoldingInstruction::X(f), &Dot { x, y }) if x > f => Dot { x: 2 * f - x, y },
+            (&FoldingInstruction::Y(f), &Dot { x, y }) if y > f => Dot { x, y: 2 * f - y },
+            (_, &Dot { x, y }) => Dot { x, y },
+        }
+    }
 }
 
-fn part_two(origami: &mut Origami) -> i64 {
-    0
+fn parse_buffer(buffer: &str) -> Result<Input> {
+    let mut dots = HashSet::new();
+    let mut instructions = VecDeque::new();
+    let mut lines = buffer.lines();
+    while let Some(line) = lines.next() {
+        if line == "" {
+            break;
+        }
+        dots.insert(Dot::from(line));
+    }
+    while let Some(line) = lines.next() {
+        instructions.push_back(FoldingInstruction::from(line));
+    }
+    Ok(Input { dots, instructions })
+}
+
+fn part_one(input: &Input) -> Result<i128> {
+    let mut origami = Origami::from(input);
+    origami.execute_instruction();
+
+    Ok(origami.count_dots())
+}
+
+fn part_two(_input: &Input) -> Result<i128> {
+    Ok(0)
+}
+
+fn main() -> Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let input = parse_buffer(&buffer)?;
+    println!("Part one: {}", part_one(&input)?);
+    println!("Part two: {}", part_two(&input)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    lazy_static! {
+        static ref TEST: Input = read_from_file("test.txt");
+        static ref INPUT: Input = read_from_file("input.txt");
+    }
+
+    fn read_from_file(filename: &str) -> Input {
+        let buffer = fs::read_to_string(filename)
+            .unwrap_or_else(|msg| panic!("error reading {}: {}", filename, msg));
+
+        parse_buffer(&buffer).unwrap_or_else(|msg| panic!("error parsing {}: {}", filename, msg))
+    }
+
+    #[test]
+    fn test_one() -> Result<()> {
+        let answer = super::part_one(&TEST)?;
+        assert_eq!(answer, 17);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_one --nocapture" to print the time
+    #[test]
+    fn part_one() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_one(&INPUT)?;
+        eprintln!("Part one took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 751);
+        Ok(())
+    }
+
+    #[test]
+    fn test_two() -> Result<()> {
+        let answer = super::part_two(&TEST)?;
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_two --nocapture" to print the time
+    #[test]
+    fn part_two() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_two(&INPUT)?;
+        eprintln!("Part two took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 0);
+        Ok(())
+    }
 }
