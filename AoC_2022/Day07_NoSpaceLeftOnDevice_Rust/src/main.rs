97e6d68c9d8db12b65e4c552307602c59f8beef2
Jörg Planner <joerg.planner@methodpark.de>
2022-12-07 10:10:59 +0100

    [2022] day07 small refactorings

--- AoC_2022/Day07_NoSpaceLeftOnDevice_Rust/src/main.rs
+++ AoC_2022/Day07_NoSpaceLeftOnDevice_Rust/src/main.rs
@@ -16,8 +16,8 @@ struct File {
 struct Directory {
     name: String,
     files: HashMap<String, File>,
-    parent: Weak<RefCell<Directory>>,
-    subdirectories: HashMap<String, Rc<RefCell<Directory>>>,
+    parent_dir: Weak<RefCell<Directory>>,
+    sub_dirs: HashMap<String, Rc<RefCell<Directory>>>,
 }
 
 impl Directory {
@@ -25,20 +25,20 @@ impl Directory {
         Directory {
             name,
             files: HashMap::new(),
-            parent: Weak::new(),
-            subdirectories: HashMap::new(),
+            parent_dir: Weak::new(),
+            sub_dirs: HashMap::new(),
         }
     }
-    fn get_total_size(&self) -> usize {
+    fn total_size(&self) -> usize {
         let mut sum: usize = self.files.iter().map(|(_, f)| f.size).sum();
-        for (_, dir) in self.subdirectories.iter() {
-            sum += dir.borrow().get_total_size();
+        for (_, dir) in self.sub_dirs.iter() {
+            sum += dir.borrow().total_size();
         }
         sum
     }
-    fn print_tree(&self, prefix_spaces: usize) {
-        let prefix: String = vec![' '; prefix_spaces].iter().collect();
-        let prefix_contained = format!("{}{}", prefix, "  ");
+    fn print(&self, prefix_spaces: usize) {
+        let prefix = " ".repeat(prefix_spaces);
+        let prefix_contained = " ".repeat(prefix_spaces + 2);
         println!("{}{} (dir)", &prefix, self.name);
         for (_, file) in self.files.iter() {
             println!(
@@ -46,14 +46,14 @@ impl Directory {
                 &prefix_contained, file.name, file.size
             );
         }
-        for (_, dir) in self.subdirectories.iter() {
-            dir.borrow().print_tree(prefix_spaces + 2);
+        for (_, dir) in self.sub_dirs.iter() {
+            dir.borrow().print(prefix_spaces + 2);
         }
     }
     fn sum_of_smaller_dirs(&self, inclusive_threshold: usize) -> usize {
         let mut sum = 0;
-        for (_, subdir) in self.subdirectories.iter() {
-            let subdir_size = subdir.borrow().get_total_size();
+        for (_, subdir) in self.sub_dirs.iter() {
+            let subdir_size = subdir.borrow().total_size();
             if subdir_size <= inclusive_threshold {
                 sum += subdir_size;
             }
@@ -62,16 +62,18 @@ impl Directory {
         sum
     }
     fn get_dir_sizes_suitable_for_deletion(&self, min_to_del: usize, vec_to_fill: &mut Vec<usize>) {
-        let size = self.get_total_size();
+        let size = self.total_size();
         if size >= min_to_del {
             vec_to_fill.push(size);
         }
-        for (_, subdir) in self.subdirectories.iter() {
+        for (_, subdir) in self.sub_dirs.iter() {
             subdir
                 .borrow()
                 .get_dir_sizes_suitable_for_deletion(min_to_del, vec_to_fill);
         }
     }
+    // fn get_foo(w: Weak<RefCell<Directory>>) -> Directory {
+    // }
 }
 
 struct TaskData {
@@ -87,20 +89,17 @@ fn parse_input(input: &str) -> Result<TaskData> {
     }
     let root = Rc::new(RefCell::new(Directory::new(String::from("/"))));
     let mut current = Rc::downgrade(&root);
-    let mut line_iter = input.lines();
-    line_iter.next(); // skip first line which always should be $ cd /
-    for line in line_iter {
+    for line in input.lines().skip(1) {
+        let rc_current = current.upgrade().unwrap();
         if let Some(caps) = CD_RE.captures(line) {
             // change current
             match &caps[1] {
                 ".." => {
-                    let rc = current.upgrade().unwrap();
-                    let parent = &rc.borrow().parent.upgrade().unwrap();
-                    current = Rc::downgrade(&parent);
+                    let parent = &rc_current.borrow().parent_dir;
+                    current = Weak::clone(&parent);
                 }
                 name => {
-                    let rc = current.upgrade().unwrap();
-                    let subdir = &rc.borrow().subdirectories[name];
+                    let subdir = &rc_current.borrow().sub_dirs[name];
                     current = Rc::downgrade(&subdir);
                 }
             }
@@ -110,15 +109,17 @@ fn parse_input(input: &str) -> Result<TaskData> {
             // add dir to current
             let name = String::from(&caps[1]);
             let new_dir = Rc::new(RefCell::new(Directory::new(name.clone())));
-            let rc = current.upgrade().unwrap();
-            new_dir.borrow_mut().parent = Rc::downgrade(&rc);
-            rc.borrow_mut().subdirectories.insert(name.clone(), new_dir);
+            new_dir.borrow_mut().parent_dir = Rc::downgrade(&rc_current);
+            rc_current
+                .borrow_mut()
+                .sub_dirs
+                .insert(name.clone(), new_dir);
         } else if let Some(caps) = FILE_RE.captures(line) {
             // add file to current
             let size = usize::from_str_radix(&caps[1], 10).unwrap();
             let name = String::from(&caps[2]);
-            let rc = current.upgrade().unwrap();
-            rc.borrow_mut()
+            rc_current
+                .borrow_mut()
                 .files
                 .insert(name.clone(), File { name, size });
         } else {
@@ -131,24 +132,23 @@ fn parse_input(input: &str) -> Result<TaskData> {
 
 fn part_one(input: &str) -> Result<usize> {
     let TaskData { root } = parse_input(input)?;
-    root.borrow().print_tree(0);
+    root.borrow().print(0);
     let answer = root.borrow().sum_of_smaller_dirs(100000);
     Ok(answer)
 }
 
 fn part_two(input: &str) -> Result<usize> {
     let TaskData { root } = parse_input(input)?;
-    let total_size = root.borrow().get_total_size();
+    let total_size = root.borrow().total_size();
     let total_disk = 70000000;
     let need_free = 30000000;
     let current_free = total_disk - total_size;
-    let deletion_size = need_free - current_free;
-    let mut suit = Vec::new();
+    let min_space_to_free = need_free - current_free;
+    let mut suitable_for_deletion = Vec::new();
     root.borrow()
-        .get_dir_sizes_suitable_for_deletion(deletion_size, &mut suit);
-    suit.sort();
-    println!("{:?}", suit);
-    Ok(suit[0])
+        .get_dir_sizes_suitable_for_deletion(min_space_to_free, &mut suitable_for_deletion);
+    suitable_for_deletion.sort();
+    Ok(suitable_for_deletion[0])
 }
 
 fn main() -> Result<()> {
