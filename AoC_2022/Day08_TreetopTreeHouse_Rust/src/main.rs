de189c7bf913f1c3868c0cc6f3772b28099d821c
Jörg Planner <joerg.planner@methodpark.de>
2022-12-08 06:40:31 +0100

    [2022] day08 part1

---
+++ AoC_2022/Day08_TreetopTreeHouse_Rust/src/main.rs
@@ -0,0 +1,178 @@
+use anyhow::Result;
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+struct TaskData {
+    tree_grid: Vec<Vec<i128>>,
+}
+
+fn max(a: i128, b: i128) -> i128 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+impl TaskData {
+    fn count_visible(&self) -> i128 {
+        let rows = self.tree_grid.len();
+        let cols = self.tree_grid[0].len();
+        // count from each side, don't count twice
+        let mut counted: HashSet<(usize, usize)> = HashSet::new();
+        let mut total = 0;
+        // top
+        for col in 0..cols {
+            let mut min_size = -1;
+            for row in 0..rows {
+                // already found largest tree
+                if counted.contains(&(row, col)) {
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    continue;
+                }
+                if self.tree_grid[row][col] > min_size {
+                    total += 1;
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    counted.insert((row, col));
+                }
+            }
+        }
+        // bottom
+        for col in 0..cols {
+            let mut min_size = -1;
+            for row in (0..rows).rev() {
+                // already found largest tree
+                if counted.contains(&(row, col)) {
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    continue;
+                }
+                if self.tree_grid[row][col] > min_size {
+                    total += 1;
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    counted.insert((row, col));
+                }
+            }
+        }
+        // left
+        for row in 0..rows {
+            let mut min_size = -1;
+            for col in 0..cols {
+                if counted.contains(&(row, col)) {
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    continue;
+                }
+                if self.tree_grid[row][col] > min_size {
+                    total += 1;
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    counted.insert((row, col));
+                }
+            }
+        }
+        // right
+        for row in 0..rows {
+            let mut min_size = -1;
+            for col in (0..cols).rev() {
+                if counted.contains(&(row, col)) {
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    continue;
+                }
+                if self.tree_grid[row][col] > min_size {
+                    total += 1;
+                    min_size = max(min_size, self.tree_grid[row][col]);
+                    counted.insert((row, col));
+                }
+            }
+        }
+        let debug: Vec<(usize, usize)> = counted
+            .into_iter()
+            .filter(|&(a, b)| a != 0 && a != rows - 1 && b != 0 && b != cols - 1)
+            .collect();
+        println!("{:?}", debug);
+        total
+    }
+}
+
+fn parse_input(input: &str) -> Result<TaskData> {
+    let mut tree_grid = Vec::new();
+    for line in input.lines() {
+        let line_heights: Vec<i128> = line
+            .chars()
+            .map(|c| (c as u8 - '0' as u8) as i128)
+            .collect();
+        tree_grid.push(line_heights);
+    }
+    Ok(TaskData { tree_grid })
+}
+
+fn part_one(input: &str) -> Result<i128> {
+    let data = parse_input(input)?;
+    Ok(data.count_visible())
+}
+
+fn part_two(input: &str) -> Result<i128> {
+    let _ = parse_input(input)?;
+    Ok(-1)
+}
+
+fn main() -> Result<()> {
+    for i in (0..10).rev() {
+        println!("{}", i);
+    }
+    println!("{}", part_one("00000\n00300\n00000")?);
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part one: {}", part_one(&input)?);
+    println!("Part two: {}", part_two(&input)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lazy_static::lazy_static;
+    use std::fs;
+
+    lazy_static! {
+        static ref TEST: String = read_from_file("test.txt");
+        static ref INPUT: String = read_from_file("input.txt");
+    }
+
+    fn read_from_file(filename: &str) -> String {
+        fs::read_to_string(filename)
+            .unwrap_or_else(|msg| panic!("error reading {}: {}", filename, msg))
+    }
+
+    #[test]
+    fn test_one() -> Result<()> {
+        let answer = super::part_one(&TEST)?;
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_one --nocapture" to print the time
+    #[test]
+    fn part_one() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_one(&INPUT)?;
+        eprintln!("Part one took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_two() -> Result<()> {
+        let answer = super::part_two(&TEST)?;
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_two --nocapture" to print the time
+    #[test]
+    fn part_two() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_two(&INPUT)?;
+        eprintln!("Part two took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+}
