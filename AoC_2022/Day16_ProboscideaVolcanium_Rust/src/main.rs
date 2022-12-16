48f660093df81f18e753ce8f1d3c5ed1ad85525e
Jörg Planner <joerg.planner@methodpark.de>
2022-12-16 07:48:42 +0100

    [2022] day16 temp commit

---
+++ AoC_2022/Day16_ProboscideaVolcanium_Rust/src/main.rs
@@ -0,0 +1,194 @@
+use anyhow::Result;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::io::{self, Read};
+
+static TOTAL_TIME: i128 = 30;
+
+fn open(flow: i128, remaining: i128) -> i128 {
+    // open starts at_minute, takes one minute then releases until end
+    let released = flow * (remaining - 1);
+    released
+}
+
+#[derive(Debug)]
+struct TaskData {
+    nodes: HashMap<String, i128>,
+    adjacency: HashMap<String, Vec<String>>,
+    best_release: HashMap<(String, i128), i128>, // (node, time) -> released
+}
+impl TaskData {
+    fn release_the_most(&mut self, start: String) -> i128 {
+        self.bfs_with_prune(start);
+        self.best_release
+            .iter()
+            .map(|(_, released)| *released)
+            .max()
+            .unwrap()
+    }
+    fn do_prune(&self, node: String, remaining_time: i128, released: i128) -> bool {
+        // let key = (node, remaining_time);
+        // if self.best_release.contains_key(&key) {
+        //     self.best_release[&key] > released
+        // } else {
+        //     false
+        // }
+        false
+    }
+    fn update_best(&mut self, node: String, remaining: i128, released: i128) {
+        let key = (node, remaining);
+        if self.best_release.contains_key(&key) {
+            if self.best_release[&key] < released {
+                self.best_release.insert(key, released);
+            }
+        } else {
+            self.best_release.insert(key, released);
+        }
+    }
+    fn bfs_with_prune(&mut self, start: String) {
+        let mut queue: VecDeque<(String, i128, i128, HashSet<String>)> = VecDeque::new(); // at_min, released, remaining
+        queue.push_back((start, 30, 0, HashSet::new()));
+        while let Some((node, remaining, released, opened)) = queue.pop_front() {
+            if remaining <= 0 {
+                continue;
+            } // no time left => nothing to do
+              // try open
+            let flow = self.nodes[&node];
+            if !opened.contains(&node) && flow > 0 {
+                let new_remaining = remaining - 1; // open costs 1
+                let new_released = released + open(flow, remaining);
+                let mut new_opened = opened.clone();
+                new_opened.insert(node.clone());
+                if !self.do_prune(node.clone(), new_remaining, new_released) {
+                    self.update_best(node.clone(), new_remaining, new_released);
+                    queue.push_back((node.clone(), new_remaining, new_released, new_opened));
+                }
+            }
+            // insert neighbors
+            let neighs: Vec<String> = self.adjacency[&node].clone();
+            for neigh in neighs {
+                let new_remaining = remaining - 1; // moving costs 1
+                let new_released = released;
+                let new_opened = opened.clone();
+
+                if !self.do_prune(neigh.clone(), new_remaining, new_released) {
+                    self.update_best(neigh.clone(), new_remaining, new_released);
+                    queue.push_back((neigh.clone(), new_remaining, new_released, new_opened));
+                }
+            }
+        }
+    }
+}
+
+//           BB
+//           |
+//      CC---AA---DD
+//           |
+//           EE
+//
+// graph with costs between edges
+// each node has open(time) -> released
+// somehow avoid repeating states e.g. nodes concat + time
+// same node but with less time and less released => prune
+// never open valves with flow 0 -> just treat as opened from the start
+
+fn parse_input(input: &str) -> Result<TaskData> {
+    lazy_static! {
+        static ref FLOW: Regex = Regex::new(r"flow rate=(\d+)").unwrap();
+        static ref NODES: Regex = Regex::new(r"([A-Z][A-Z])").unwrap();
+    }
+    let mut nodes = HashMap::new();
+    let mut adjacency = HashMap::new();
+    let mut best_release = HashMap::new();
+    for line in input.lines() {
+        let flow = i128::from_str_radix(&FLOW.captures(line).unwrap()[1], 10).unwrap();
+        let nodes_cap: Vec<String> = NODES
+            .captures_iter(line)
+            .map(|c| String::from(&c[0]))
+            .collect();
+        let name = nodes_cap[0].clone();
+        nodes.insert(name.clone(), flow);
+        let neighs: Vec<String> = nodes_cap.iter().skip(1).map(|s| s.clone()).collect();
+        adjacency.insert(name.clone(), neighs);
+        best_release.insert((name.clone(), 30), 0);
+    }
+    Ok(TaskData {
+        nodes,
+        adjacency,
+        best_release,
+    })
+}
+
+fn part_one(input: &str) -> Result<i128> {
+    let mut data = parse_input(input)?;
+    println!("{:?}", data);
+    let answer = data.release_the_most(String::from("AA"));
+    println!("");
+    println!("{:?}", data);
+    Ok(answer)
+}
+
+fn part_two(input: &str) -> Result<i128> {
+    let _ = parse_input(input)?;
+    Ok(-1)
+}
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    println!("Part one: {}", part_one(&input)?);
+    println!("Part two: {}", part_two(&input)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lazy_static::lazy_static;
+    use std::fs;
+
+    lazy_static! {
+        static ref TEST: String = read_from_file("test.txt");
+        static ref INPUT: String = read_from_file("input.txt");
+    }
+
+    fn read_from_file(filename: &str) -> String {
+        fs::read_to_string(filename)
+            .unwrap_or_else(|msg| panic!("error reading {}: {}", filename, msg))
+    }
+
+    #[test]
+    fn test_one() -> Result<()> {
+        let answer = super::part_one(&TEST)?;
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_one --nocapture" to print the time
+    #[test]
+    fn part_one() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_one(&INPUT)?;
+        eprintln!("Part one took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_two() -> Result<()> {
+        let answer = super::part_two(&TEST)?;
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+
+    // Use "cargo test --release -- part_two --nocapture" to print the time
+    #[test]
+    fn part_two() -> Result<()> {
+        let t = std::time::Instant::now();
+        let answer = super::part_two(&INPUT)?;
+        eprintln!("Part two took {:0.2?}", t.elapsed());
+        assert_eq!(answer, 0);
+        Ok(())
+    }
+}
