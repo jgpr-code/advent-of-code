e3b993dea29ea109f6037d73278a8f9dbc739628
Jörg Planner <joerg.planner@methodpark.de>
2022-12-16 08:15:13 +0100

    [2022] day16 part 1

--- AoC_2022/Day16_ProboscideaVolcanium_Rust/src/main.rs
+++ AoC_2022/Day16_ProboscideaVolcanium_Rust/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{BTreeSet, HashMap, VecDeque};
 use std::io::{self, Read};
 
 static TOTAL_TIME: i128 = 30;
@@ -16,8 +16,15 @@ fn open(flow: i128, remaining: i128) -> i128 {
 struct TaskData {
     nodes: HashMap<String, i128>,
     adjacency: HashMap<String, Vec<String>>,
-    best_release: HashMap<(String, i128), i128>, // (node, time) -> released
+    best_release: HashMap<(String, i128, BTreeSet<String>), i128>, // (node, time, opened) -> released
 }
+struct QueueElem {
+    node: String,
+    remaining: i128,
+    released: i128,
+    opened: BTreeSet<String>,
+}
+
 impl TaskData {
     fn release_the_most(&mut self, start: String) -> i128 {
         self.bfs_with_prune(start);
@@ -27,17 +34,29 @@ impl TaskData {
             .max()
             .unwrap()
     }
-    fn do_prune(&self, node: String, remaining_time: i128, released: i128) -> bool {
-        // let key = (node, remaining_time);
-        // if self.best_release.contains_key(&key) {
-        //     self.best_release[&key] > released
-        // } else {
-        //     false
-        // }
-        false
+    fn do_prune(
+        &self,
+        node: String,
+        remaining_time: i128,
+        opened: BTreeSet<String>,
+        released: i128,
+    ) -> bool {
+        let key = (node, remaining_time, opened);
+        if self.best_release.contains_key(&key) {
+            self.best_release[&key] >= released
+        } else {
+            false
+        }
+        //false
     }
-    fn update_best(&mut self, node: String, remaining: i128, released: i128) {
-        let key = (node, remaining);
+    fn update_best(
+        &mut self,
+        node: String,
+        remaining: i128,
+        opened: BTreeSet<String>,
+        released: i128,
+    ) {
+        let key = (node, remaining, opened);
         if self.best_release.contains_key(&key) {
             if self.best_release[&key] < released {
                 self.best_release.insert(key, released);
@@ -47,34 +66,69 @@ impl TaskData {
         }
     }
     fn bfs_with_prune(&mut self, start: String) {
-        let mut queue: VecDeque<(String, i128, i128, HashSet<String>)> = VecDeque::new(); // at_min, released, remaining
-        queue.push_back((start, 30, 0, HashSet::new()));
-        while let Some((node, remaining, released, opened)) = queue.pop_front() {
-            if remaining <= 0 {
+        let mut queue: VecDeque<QueueElem> = VecDeque::new(); // at_min, released, remaining
+        queue.push_back(QueueElem {
+            node: start,
+            remaining: 30,
+            released: 0,
+            opened: BTreeSet::new(),
+        }); // node, remaining, released, opened, opened_str
+        while let Some(elem) = queue.pop_front() {
+            if elem.remaining <= 0 {
                 continue;
             } // no time left => nothing to do
               // try open
-            let flow = self.nodes[&node];
-            if !opened.contains(&node) && flow > 0 {
-                let new_remaining = remaining - 1; // open costs 1
-                let new_released = released + open(flow, remaining);
-                let mut new_opened = opened.clone();
-                new_opened.insert(node.clone());
-                if !self.do_prune(node.clone(), new_remaining, new_released) {
-                    self.update_best(node.clone(), new_remaining, new_released);
-                    queue.push_back((node.clone(), new_remaining, new_released, new_opened));
+            let flow = self.nodes[&elem.node];
+            if !elem.opened.contains(&elem.node) && flow > 0 {
+                let new_remaining = elem.remaining - 1; // open costs 1
+                let new_released = elem.released + open(flow, elem.remaining);
+                let mut new_opened = elem.opened.clone();
+                new_opened.insert(elem.node.clone());
+                if !self.do_prune(
+                    elem.node.clone(),
+                    new_remaining,
+                    new_opened.clone(),
+                    new_released,
+                ) {
+                    self.update_best(
+                        elem.node.clone(),
+                        new_remaining,
+                        new_opened.clone(),
+                        new_released,
+                    );
+                    queue.push_back(QueueElem {
+                        node: elem.node.clone(),
+                        remaining: new_remaining,
+                        released: new_released,
+                        opened: new_opened,
+                    });
                 }
             }
             // insert neighbors
-            let neighs: Vec<String> = self.adjacency[&node].clone();
+            let neighs: Vec<String> = self.adjacency[&elem.node].clone();
             for neigh in neighs {
-                let new_remaining = remaining - 1; // moving costs 1
-                let new_released = released;
-                let new_opened = opened.clone();
+                let new_remaining = elem.remaining - 1; // moving costs 1
+                let new_released = elem.released;
+                let new_opened = elem.opened.clone();
 
-                if !self.do_prune(neigh.clone(), new_remaining, new_released) {
-                    self.update_best(neigh.clone(), new_remaining, new_released);
-                    queue.push_back((neigh.clone(), new_remaining, new_released, new_opened));
+                if !self.do_prune(
+                    neigh.clone(),
+                    new_remaining,
+                    new_opened.clone(),
+                    new_released,
+                ) {
+                    self.update_best(
+                        neigh.clone(),
+                        new_remaining,
+                        new_opened.clone(),
+                        new_released,
+                    );
+                    queue.push_back(QueueElem {
+                        node: neigh.clone(),
+                        remaining: new_remaining,
+                        released: new_released,
+                        opened: new_opened.clone(),
+                    });
                 }
             }
         }
@@ -111,7 +165,7 @@ fn parse_input(input: &str) -> Result<TaskData> {
         nodes.insert(name.clone(), flow);
         let neighs: Vec<String> = nodes_cap.iter().skip(1).map(|s| s.clone()).collect();
         adjacency.insert(name.clone(), neighs);
-        best_release.insert((name.clone(), 30), 0);
+        best_release.insert((name.clone(), 30, BTreeSet::new()), 0);
     }
     Ok(TaskData {
         nodes,
@@ -122,10 +176,10 @@ fn parse_input(input: &str) -> Result<TaskData> {
 
 fn part_one(input: &str) -> Result<i128> {
     let mut data = parse_input(input)?;
-    println!("{:?}", data);
+    //println!("{:?}", data);
     let answer = data.release_the_most(String::from("AA"));
-    println!("");
-    println!("{:?}", data);
+    //println!("");
+    //println!("{:?}", data);
     Ok(answer)
 }
 
