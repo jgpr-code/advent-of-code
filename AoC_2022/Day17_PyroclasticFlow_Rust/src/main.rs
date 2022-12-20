fde7867ef405c53ad39566ac82e688a286d42ead
Jörg Planner <joerg.planner@methodpark.de>
2022-12-20 17:55:33 +0100

    [2022] day17 commit before rework

--- AoC_2022/Day17_PyroclasticFlow_Rust/src/main.rs
+++ AoC_2022/Day17_PyroclasticFlow_Rust/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::cmp;
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 use std::io::{self, Read};
 
 // 7 wide
@@ -28,16 +28,17 @@ struct Pos {
 }
 
 impl Pos {
-    fn jet(&self, jet: Jet) -> Pos {
+    fn jet(&self, jet: char) -> Pos {
         match jet {
-            Jet::Left => Pos {
+            '<' => Pos {
                 x: self.x - 1,
                 y: self.y,
             },
-            Jet::Right => Pos {
+            '>' => Pos {
                 x: self.x + 1,
                 y: self.y,
             },
+            _ => panic!("not a jet"),
         }
     }
     fn fall(&self) -> Pos {
@@ -48,72 +49,30 @@ impl Pos {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Jet {
-    Left,
-    Right,
-}
-
-impl From<char> for Jet {
-    fn from(c: char) -> Self {
-        match c {
-            '<' => Jet::Left,
-            '>' => Jet::Right,
-            _ => panic!("wrong jet char"),
-        }
-    }
-}
-
-struct TaskData {
+struct RockTetris {
     rocks_index: usize,
     rocks: Vec<HashSet<Pos>>,
-    highest: i128,
     jets_index: usize,
-    jets: Vec<Jet>,
+    jets: Vec<char>,
+    // ground is always 0 in the coordinates however the real height is obtained by adding ground_offset
+    highest: i128,
+    ground_offset: i128,
     rested: HashSet<Pos>,
-    current_ground: i128, // the highest fully filled line
 }
 
-impl TaskData {
-    fn debug(&self, current: Option<&HashSet<Pos>>) {
-        for y in (0..=20).rev() {
-            for x in 0..=8 {
-                if y == 0 {
-                    print!("-");
-                } else if x == 0 || x == 8 {
-                    print!("|");
-                } else {
-                    if self.rested.contains(&Pos { x, y }) {
-                        print!("#");
-                    } else {
-                        if let Some(falling) = current {
-                            if falling.contains(&Pos { x, y }) {
-                                print!("@");
-                            } else {
-                                print!(".");
-                            }
-                        } else {
-                            print!(".");
-                        }
-                    }
-                }
-            }
-            println!("");
-        }
-    }
+impl RockTetris {
     fn intersect(&self, rock: &HashSet<Pos>) -> bool {
-        // intersect with ground
         let intersect = rock.intersection(&self.rested).count();
         if intersect > 0 {
             return true;
         }
         let intersect = rock
             .iter()
-            .filter(|p| p.x == 0 || p.x == 8 || p.y == self.current_ground)
+            .filter(|p| p.x == 0 || p.x == 8 || p.y == self.ground_offset)
             .count();
         intersect > 0
     }
-    fn jet(jet: Jet, rock: &HashSet<Pos>) -> HashSet<Pos> {
+    fn jet(jet: char, rock: &HashSet<Pos>) -> HashSet<Pos> {
         rock.iter().map(|p| p.jet(jet)).collect()
     }
     fn fall(rock: &HashSet<Pos>) -> HashSet<Pos> {
@@ -134,7 +93,6 @@ impl TaskData {
     }
     fn handle_rock(&mut self, rock: &HashSet<Pos>) {
         let mut current = rock.clone();
-        // self.debug(Some(&current));
         loop {
             let jet = self.jets[self.jets_index];
             self.jets_index = (self.jets_index + 1) % self.jets.len();
@@ -142,18 +100,13 @@ impl TaskData {
             if !self.intersect(&jetted) {
                 current = jetted.clone();
             }
-            // println!("after jet");
-            // self.debug(Some(&current));
+
             let falled = Self::fall(&current);
-            // println!("after fall (before correct)");
-            // self.debug(Some(&falled));
+
             if self.intersect(&falled) {
                 self.rested = self.rested.union(&current).map(|&p| p).collect();
                 self.highest = cmp::max(self.highest, current.iter().map(|p| p.y).max().unwrap());
                 self.cleanup(&current);
-                //self.cleanup2(); doesn't work
-                //self.cleanup_bfs(); doesn't help or doesn't work
-                self.heuristic_cleanup();
                 return;
             } else {
                 current = falled.clone();
@@ -179,57 +132,10 @@ impl TaskData {
         }
     }
     fn discard_rested_at_and_below(&mut self, y: i128) {
-        self.current_ground = y;
+        self.ground_offset = y;
         self.rested = self.rested.iter().map(|&p| p).filter(|p| p.y > y).collect();
     }
-    fn cleanup2(&mut self) {
-        // the idea here is to find the largest y for each x and then use the min of those y's and discard below
-        let mut largest_y: Vec<i128> = vec![0; 7]; // 1 indexed
-        for pos in self.rested.iter() {
-            largest_y[(pos.x - 1) as usize] = cmp::max(largest_y[(pos.x - 1) as usize], pos.y);
-        }
-        let safe_to_discard = *largest_y.iter().min().unwrap();
-        self.discard_rested_at_and_below(safe_to_discard);
-    }
-    fn cleanup_bfs(&mut self) {
-        let lowest_reachable = self.find_lowest_reachable_bfs();
-        self.discard_rested_at_and_below(lowest_reachable - 1);
-    }
-    fn find_lowest_reachable_bfs(&self) -> i128 {
-        // just start one above the highest
-        let start = Pos {
-            x: 0,
-            y: self.highest + 1,
-        };
-        let mut queue = VecDeque::new();
-        let mut visited = HashSet::new();
-        queue.push_back(start);
-        visited.insert(start);
-        while let Some(pos) = queue.pop_front() {
-            // try down left right
-            let dx = vec![-1, 0, 1];
-            let dy = vec![0, -1, 0];
-            for i in 0..3 {
-                let nx = pos.x + dx[i];
-                let ny = pos.y + dy[i];
-                if nx == 0 || nx == 8 || ny == 0 {
-                    continue;
-                }
-                let npos = Pos { x: nx, y: ny };
-                if visited.contains(&npos) || self.rested.contains(&npos) {
-                    continue;
-                }
-                queue.push_back(npos);
-                visited.insert(npos);
-            }
-        }
-        visited.iter().map(|p| p.y).min().unwrap()
-    }
-    fn heuristic_cleanup(&mut self) {
-        // the idea is that after n rocks so many lines have to be filled even with perfect play
-        // 4 * tiles (tile has at least 4) / 7
-        //
-    }
+
     fn run_n_rocks(&mut self, n: i128) {
         for i in 1..=n {
             if i % 1_000_000_000 == 0 {
@@ -295,53 +201,44 @@ fn construct_rocks() -> Vec<HashSet<Pos>> {
     vec![line, cross, l_shape, down_line, block]
 }
 
-fn parse_input(input: &str) -> Result<TaskData> {
-    let jets: Vec<Vec<Jet>> = input
-        .lines()
-        .map(|l| l.chars().map(|c| Jet::from(c)).collect())
-        .collect();
+fn parse_input(input: &str) -> Result<RockTetris> {
+    let jets: Vec<char> = input.lines().nth(0).unwrap().chars().collect();
 
-    Ok(TaskData {
+    Ok(RockTetris {
         rocks_index: 0,
         rocks: construct_rocks(),
-        highest: 0,
         jets_index: 0,
-        jets: jets[0].clone(),
+        jets,
+
+        highest: 0,
+        ground_offset: 0,
         rested: HashSet::new(),
-        current_ground: 0,
     })
 }
 
 fn part_one(input: &str) -> Result<i128> {
     let mut data = parse_input(input)?;
     data.run_n_rocks(2022);
-    //data.debug(None);
-
-    let inner = 20;
-    let start: i128 = 0;
-    let end: i128 = 1_000_000_000_000;
-    for i in start..end {
-        let mut amount = 0;
-        for j in 0..inner {
-            amount += j % 1337;
-        }
-    }
-    println!("loop fin");
     Ok(data.highest)
 }
 
 fn part_two(input: &str) -> Result<i128> {
     let mut data = parse_input(input)?;
     data.run_n_rocks(1_000_000_000_000);
-    //data.debug(None);
     Ok(data.highest)
 }
 
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    println!("Part one: {}", part_one(&input)?);
-    println!("Part two: {}", part_two(&input)?);
+    let t = std::time::Instant::now();
+    let part_one = part_one(&input)?;
+    let elapsed = t.elapsed();
+    println!("Part one: {} in {:0.2?}", part_one, elapsed);
+    let t = std::time::Instant::now();
+    let part_two = part_two(&input)?;
+    let elapsed = t.elapsed();
+    println!("Part two: {} in {:0.2?}", part_two, elapsed);
     Ok(())
 }
 
@@ -395,3 +292,74 @@ mod tests {
         Ok(())
     }
 }
+
+// fn debug(&self, current: Option<&HashSet<Pos>>) {
+//     for y in (0..=20).rev() {
+//         for x in 0..=8 {
+//             if y == 0 {
+//                 print!("-");
+//             } else if x == 0 || x == 8 {
+//                 print!("|");
+//             } else {
+//                 if self.rested.contains(&Pos { x, y }) {
+//                     print!("#");
+//                 } else {
+//                     if let Some(falling) = current {
+//                         if falling.contains(&Pos { x, y }) {
+//                             print!("@");
+//                         } else {
+//                             print!(".");
+//                         }
+//                     } else {
+//                         print!(".");
+//                     }
+//                 }
+//             }
+//         }
+//         println!("");
+//     }
+// }
+
+// fn cleanup2(&mut self) {
+//     // the idea here is to find the largest y for each x and then use the min of those y's and discard below
+//     let mut largest_y: Vec<i128> = vec![0; 7]; // 1 indexed
+//     for pos in self.rested.iter() {
+//         largest_y[(pos.x - 1) as usize] = cmp::max(largest_y[(pos.x - 1) as usize], pos.y);
+//     }
+//     let safe_to_discard = *largest_y.iter().min().unwrap();
+//     self.discard_rested_at_and_below(safe_to_discard);
+// }
+// fn cleanup_bfs(&mut self) {
+//     let lowest_reachable = self.find_lowest_reachable_bfs();
+//     self.discard_rested_at_and_below(lowest_reachable - 1);
+// }
+// fn find_lowest_reachable_bfs(&self) -> i128 {
+//     // just start one above the highest
+//     let start = Pos {
+//         x: 0,
+//         y: self.highest + 1,
+//     };
+//     let mut queue = VecDeque::new();
+//     let mut visited = HashSet::new();
+//     queue.push_back(start);
+//     visited.insert(start);
+//     while let Some(pos) = queue.pop_front() {
+//         // try down left right
+//         let dx = vec![-1, 0, 1];
+//         let dy = vec![0, -1, 0];
+//         for i in 0..3 {
+//             let nx = pos.x + dx[i];
+//             let ny = pos.y + dy[i];
+//             if nx == 0 || nx == 8 || ny == 0 {
+//                 continue;
+//             }
+//             let npos = Pos { x: nx, y: ny };
+//             if visited.contains(&npos) || self.rested.contains(&npos) {
+//                 continue;
+//             }
+//             queue.push_back(npos);
+//             visited.insert(npos);
+//         }
+//     }
+//     visited.iter().map(|p| p.y).min().unwrap()
+// }
