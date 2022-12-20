fadea113fd156893ed216b9f1274b725ae399d5e
Jörg Planner <joerg.planner@methodpark.de>
2022-12-20 18:39:31 +0100

    [2022] day17 part2

--- AoC_2022/Day17_PyroclasticFlow_Rust/src/main.rs
+++ AoC_2022/Day17_PyroclasticFlow_Rust/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::cmp;
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashMap};
 use std::io::{self, Read};
 
 // 7 wide
@@ -21,7 +21,7 @@ use std::io::{self, Read};
 
 // highest rock is 0 in the beginning (floor)
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Pos {
     x: i128,
     y: i128,
@@ -51,34 +51,29 @@ impl Pos {
 
 struct RockTetris {
     rocks_index: usize,
-    rocks: Vec<HashSet<Pos>>,
+    rocks: Vec<BTreeSet<Pos>>,
     jets_index: usize,
     jets: Vec<char>,
     // ground is always 0 in the coordinates however the real height is obtained by adding ground_offset
+    total_rocks_dropped: i128,
     highest: i128,
     ground_offset: i128,
-    rested: HashSet<Pos>,
+    rested: BTreeSet<Pos>,
+    // store the encountered completions
+    // after completion with discard happened
+    // insert key: (rocks_index, jets_index, rested) value: ground_offset
+    encountered_completions: HashMap<(usize, usize, BTreeSet<Pos>), (i128, i128)>,
 }
 
 impl RockTetris {
-    fn intersect(&self, rock: &HashSet<Pos>) -> bool {
-        let intersect = rock.intersection(&self.rested).count();
-        if intersect > 0 {
-            return true;
+    fn run_n_rocks(&mut self, n: i128) {
+        while self.total_rocks_dropped < n {
+            let rock = self.next_rock();
+            self.total_rocks_dropped += 1;
+            self.handle_rock(&rock, n);
         }
-        let intersect = rock
-            .iter()
-            .filter(|p| p.x == 0 || p.x == 8 || p.y == self.ground_offset)
-            .count();
-        intersect > 0
     }
-    fn jet(jet: char, rock: &HashSet<Pos>) -> HashSet<Pos> {
-        rock.iter().map(|p| p.jet(jet)).collect()
-    }
-    fn fall(rock: &HashSet<Pos>) -> HashSet<Pos> {
-        rock.iter().map(|p| p.fall()).collect()
-    }
-    fn next_rock(&mut self) -> HashSet<Pos> {
+    fn next_rock(&mut self) -> BTreeSet<Pos> {
         let initial = &self.rocks[self.rocks_index];
         self.rocks_index = (self.rocks_index + 1) % self.rocks.len();
         let x_off = 2 + 1;
@@ -91,29 +86,49 @@ impl RockTetris {
             })
             .collect()
     }
-    fn handle_rock(&mut self, rock: &HashSet<Pos>) {
+    fn handle_rock(&mut self, rock: &BTreeSet<Pos>, n: i128) {
         let mut current = rock.clone();
+
         loop {
             let jet = self.jets[self.jets_index];
             self.jets_index = (self.jets_index + 1) % self.jets.len();
-            let jetted = Self::jet(jet, &current);
-            if !self.intersect(&jetted) {
-                current = jetted.clone();
+            let mut next = Self::jet(jet, &current);
+            if !self.intersect(&next) {
+                std::mem::swap(&mut current, &mut next);
             }
-
-            let falled = Self::fall(&current);
-
-            if self.intersect(&falled) {
+            let mut next = Self::fall(&current);
+            if self.intersect(&next) {
                 self.rested = self.rested.union(&current).map(|&p| p).collect();
                 self.highest = cmp::max(self.highest, current.iter().map(|p| p.y).max().unwrap());
-                self.cleanup(&current);
+                self.check_line_completion(&current, n);
                 return;
             } else {
-                current = falled.clone();
+                std::mem::swap(&mut current, &mut next);
             }
         }
     }
-    fn cleanup(&mut self, current: &HashSet<Pos>) {
+    fn get_heighest(&self) -> i128 {
+        self.highest + self.ground_offset
+    }
+    fn intersect(&self, rock: &BTreeSet<Pos>) -> bool {
+        let intersect = rock.intersection(&self.rested).count();
+        if intersect > 0 {
+            return true;
+        }
+        let intersect = rock
+            .iter()
+            .filter(|p| p.x == 0 || p.x == 8 || p.y == 0)
+            .count();
+        intersect > 0
+    }
+    fn jet(jet: char, rock: &BTreeSet<Pos>) -> BTreeSet<Pos> {
+        rock.iter().map(|p| p.jet(jet)).collect()
+    }
+    fn fall(rock: &BTreeSet<Pos>) -> BTreeSet<Pos> {
+        rock.iter().map(|p| p.fall()).collect()
+    }
+
+    fn check_line_completion(&mut self, current: &BTreeSet<Pos>, n: i128) {
         // check if any line where the tile landed is complete
         let mut lines_to_check = current.iter().map(|p| p.y).collect::<Vec<_>>();
         lines_to_check.sort_by(|a, b| b.cmp(a));
@@ -126,35 +141,83 @@ impl RockTetris {
             }
             if amount == 7 {
                 // line's full
-                self.discard_rested_at_and_below(y);
+                self.handle_line_completion(y, n);
                 return;
             }
         }
     }
-    fn discard_rested_at_and_below(&mut self, y: i128) {
-        self.ground_offset = y;
-        self.rested = self.rested.iter().map(|&p| p).filter(|p| p.y > y).collect();
+    fn handle_line_completion(&mut self, y: i128, n: i128) {
+        // line 1 complete => ground_offset += 1
+        self.highest -= y;
+        self.ground_offset += y;
+        self.rested = self
+            .rested
+            .iter()
+            .filter(|p| p.y > y)
+            .map(|p| Pos { x: p.x, y: p.y - y })
+            .collect();
+        let completion_key = (self.rocks_index, self.jets_index, self.rested.clone());
+        if self.encountered_completions.contains_key(&completion_key) {
+            let ground_now = self.ground_offset;
+            let rocks_now = self.total_rocks_dropped;
+            let (rocks_before, ground_before) = self.encountered_completions[&completion_key];
+            let ground_delta = ground_now - ground_before;
+            let rocks_delta = rocks_now - rocks_before;
+            println!(
+                "ground now {} vs ground before {} delta: {}",
+                ground_now, ground_before, ground_delta
+            );
+            println!(
+                "rocks now {} vs rocks before {} delta: {}",
+                rocks_now, rocks_before, rocks_delta
+            );
+            // how much can be skipped
+            // rocks_now + n * rocks_delta == n_rocks
+            let delta = (n - rocks_now) / rocks_delta;
+            self.total_rocks_dropped += delta * rocks_delta;
+            self.ground_offset += delta * ground_delta;
+        }
+        self.encountered_completions.insert(
+            completion_key,
+            (self.total_rocks_dropped, self.ground_offset),
+        );
     }
-
-    fn run_n_rocks(&mut self, n: i128) {
-        for i in 1..=n {
-            if i % 1_000_000_000 == 0 {
-                println!("rock {}", i);
+    fn debug(&self, current: Option<&BTreeSet<Pos>>) {
+        for y in (0..=20).rev() {
+            for x in 0..=8 {
+                if y == 0 {
+                    print!("-");
+                } else if x == 0 || x == 8 {
+                    print!("|");
+                } else {
+                    if self.rested.contains(&Pos { x, y }) {
+                        print!("#");
+                    } else {
+                        if let Some(falling) = current {
+                            if falling.contains(&Pos { x, y }) {
+                                print!("@");
+                            } else {
+                                print!(".");
+                            }
+                        } else {
+                            print!(".");
+                        }
+                    }
+                }
             }
-            let rock = self.next_rock();
-            self.handle_rock(&rock);
+            println!("");
         }
     }
 }
 
 // Rock shapes repeat from top to bottom
 
-fn construct_rocks() -> Vec<HashSet<Pos>> {
+fn construct_rocks() -> Vec<BTreeSet<Pos>> {
     // 0 0 is always bottom left
     // points are inserted from top to bottom and left to right (not that it matters)
 
     // ####
-    let mut line = HashSet::new();
+    let mut line = BTreeSet::new();
     line.insert(Pos { x: 0, y: 0 });
     line.insert(Pos { x: 1, y: 0 });
     line.insert(Pos { x: 2, y: 0 });
@@ -163,7 +226,7 @@ fn construct_rocks() -> Vec<HashSet<Pos>> {
     // .#.
     // ###
     // .#.
-    let mut cross = HashSet::new();
+    let mut cross = BTreeSet::new();
     cross.insert(Pos { x: 1, y: 2 });
     cross.insert(Pos { x: 0, y: 1 });
     cross.insert(Pos { x: 1, y: 1 });
@@ -173,7 +236,7 @@ fn construct_rocks() -> Vec<HashSet<Pos>> {
     // ..#
     // ..#
     // ###
-    let mut l_shape = HashSet::new();
+    let mut l_shape = BTreeSet::new();
     l_shape.insert(Pos { x: 2, y: 2 });
     l_shape.insert(Pos { x: 2, y: 1 });
     l_shape.insert(Pos { x: 0, y: 0 });
@@ -184,7 +247,7 @@ fn construct_rocks() -> Vec<HashSet<Pos>> {
     // #
     // #
     // #
-    let mut down_line = HashSet::new();
+    let mut down_line = BTreeSet::new();
     down_line.insert(Pos { x: 0, y: 3 });
     down_line.insert(Pos { x: 0, y: 2 });
     down_line.insert(Pos { x: 0, y: 1 });
@@ -192,7 +255,7 @@ fn construct_rocks() -> Vec<HashSet<Pos>> {
 
     // ##
     // ##
-    let mut block = HashSet::new();
+    let mut block = BTreeSet::new();
     block.insert(Pos { x: 0, y: 1 });
     block.insert(Pos { x: 1, y: 1 });
     block.insert(Pos { x: 0, y: 0 });
@@ -209,23 +272,25 @@ fn parse_input(input: &str) -> Result<RockTetris> {
         rocks: construct_rocks(),
         jets_index: 0,
         jets,
-
+        total_rocks_dropped: 0,
         highest: 0,
         ground_offset: 0,
-        rested: HashSet::new(),
+        rested: BTreeSet::new(),
+        encountered_completions: HashMap::new(),
     })
 }
 
 fn part_one(input: &str) -> Result<i128> {
     let mut data = parse_input(input)?;
     data.run_n_rocks(2022);
-    Ok(data.highest)
+    data.debug(None);
+    Ok(data.get_heighest())
 }
 
 fn part_two(input: &str) -> Result<i128> {
     let mut data = parse_input(input)?;
     data.run_n_rocks(1_000_000_000_000);
-    Ok(data.highest)
+    Ok(data.get_heighest())
 }
 
 fn main() -> Result<()> {
@@ -293,7 +358,7 @@ mod tests {
     }
 }
 
-// fn debug(&self, current: Option<&HashSet<Pos>>) {
+// fn debug(&self, current: Option<&BTreeSet<Pos>>) {
 //     for y in (0..=20).rev() {
 //         for x in 0..=8 {
 //             if y == 0 {
@@ -340,7 +405,7 @@ mod tests {
 //         y: self.highest + 1,
 //     };
 //     let mut queue = VecDeque::new();
-//     let mut visited = HashSet::new();
+//     let mut visited = BTreeSet::new();
 //     queue.push_back(start);
 //     visited.insert(start);
 //     while let Some(pos) = queue.pop_front() {
